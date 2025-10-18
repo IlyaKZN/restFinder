191c97c1a4b9e1e59b9d3bfa1dba2ddba9004d4f
Илья Пичугин <pichugin@trueconf.ru>
2025-10-18 12:55:44 +0300

    wip

--- src/screens/Tournament/mocks.ts
+++ src/screens/Tournament/mocks.ts
@@ -1,3 +1,20 @@
+import q11Icon from '@/images/1_1.jpg'
+import q13Icon from '@/images/1_3.jpg'
+import q21Icon from '@/images/2_1.jpg'
+import q22Icon from '@/images/2_2.jpg'
+import q41Icon from '@/images/4_1.jpg'
+import q42Icon from '@/images/4_2.jpg'
+import q43Icon from '@/images/4_3.jpg'
+import q31Icon from '@/images/3_1.jpg'
+import q51Icon from '@/images/5_1.jpg'
+import q62Icon from '@/images/6_2.jpg'
+import q63Icon from '@/images/6_3.jpg'
+import q73Icon from '@/images/7_3.jpg'
+import q81Icon from '@/images/8_1.jpg'
+import q82Icon from '@/images/8_2.jpg'
+import q83Icon from '@/images/8_3.jpg'
+import q93Icon from '@/images/9_3.jpg'
+
 const restaurants = [
   {
     id: 1,
@@ -5,7 +22,7 @@ const restaurants = [
     cuisines: ['Европейская', 'Паб-кухня'],
     avgCheck: 950,
     minutesFromMetro: 6,
-    images: ['@/images/1_1.jpg', '@/images/1_3.jpg'],
+    images: [q11Icon, q13Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -22,7 +39,7 @@ const restaurants = [
     cuisines: ['Японская', 'Морепродукты'],
     avgCheck: 0,
     minutesFromMetro: 4,
-    images: ['@/images/2_1.jpg', '@/images/2_2.jpg'],
+    images: [q21Icon, q22Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -39,7 +56,7 @@ const restaurants = [
     cuisines: ['Итальянская', 'Паста-бар'],
     avgCheck: 1700,
     minutesFromMetro: 8,
-    images: ['@/images/4_1.jpg', '@/images/4_2.jpg', '@/images/4_3.jpg'],
+    images: [q41Icon, q42Icon, q43Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -56,7 +73,7 @@ const restaurants = [
     cuisines: ['Русская', 'Вегетарианская'],
     avgCheck: 450,
     minutesFromMetro: 2,
-    images: ['@/images/3_1.jpg'],
+    images: [q31Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -73,7 +90,7 @@ const restaurants = [
     cuisines: ['Кавказская', 'Гриль'],
     avgCheck: 1200,
     minutesFromMetro: 12,
-    images: ['@/images/5_1.jpg'],
+    images: [q51Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -90,7 +107,7 @@ const restaurants = [
     cuisines: ['Веганская', 'Здоровая еда'],
     avgCheck: 800,
     minutesFromMetro: 5,
-    images: ['@/images/6_2.jpg', '@/images/6_3.jpg'],
+    images: [q62Icon, q63Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -107,7 +124,7 @@ const restaurants = [
     cuisines: ['Мексиканская', 'Фаст-кэжуал'],
     avgCheck: 700,
     minutesFromMetro: 3,
-    images: ['@/images/7_3.jpg'],
+    images: [q73Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -124,7 +141,7 @@ const restaurants = [
     cuisines: ['Кондитерская', 'Кафе'],
     avgCheck: 600,
     minutesFromMetro: 7,
-    images: ['@/images/8_1.jpg', '@/images/8_2.jpg', '@/images/8_3.jpg'],
+    images: [q81Icon, q82Icon, q83Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -141,7 +158,7 @@ const restaurants = [
     cuisines: ['Стейк-хаус', 'Винный бар'],
     avgCheck: 2800,
     minutesFromMetro: 10,
-    images: ['@/images/9_3.jpg'],
+    images: [q93Icon],
     schedule: {
       monday: '09:00-23:00',
       tuesday: '09:00-23:00',
@@ -154,4 +171,4 @@ const restaurants = [
   },
 ]
 
-export default restaurants;
+export default restaurants
--- src/screens/Vibes/index.vue
+++ src/screens/Vibes/index.vue
@@ -2,7 +2,7 @@
   <div class="block"
   :style="{
     background: `linear-gradient(180deg,rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.8) 80%, rgba(0, 0, 0, 0.9) 100%),
-      url('@/images/background.avif')`,
+      url('${background}')`,
   }">
     <div class="rows-block">
       <div :key="index" v-for="(vibes, index) in vibesArrays" class="row">
@@ -32,6 +32,7 @@
 <script lang="ts" setup>
   import { ref } from 'vue';
   import { useRouter } from 'vue-router';
+  import background from '@/images/background.avif';
 
 
   defineOptions({
